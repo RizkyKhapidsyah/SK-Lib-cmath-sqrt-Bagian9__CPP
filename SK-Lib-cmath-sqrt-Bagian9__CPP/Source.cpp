cbc1783ee6f2a00ff207740c34b43947eb0dabb7
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-07-09 19:41:11 +0700

    Add project files.

---
+++ SK-Lib-cmath-sqrt-Bagian9__CPP/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+	long x = 6000;
+	double hasil = sqrt(x);
+	cout << "Akar kuadrat dari " << x << " adalah " << hasil << endl;
+
+	_getch();
+	return 0;
+}
